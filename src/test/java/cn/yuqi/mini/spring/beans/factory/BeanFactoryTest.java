ccfd621ccad3906cb711043c6c716e99c1909fb2
qiyu <1186387362@qq.com>
2026-01-27 20:23:37 +0800

    添加接口，测试单例测试

--- src/test/java/cn/yuqi/mini/spring/beans/factory/BeanFactoryTest.java
+++ src/test/java/cn/yuqi/mini/spring/beans/factory/BeanFactoryTest.java
@@ -19,6 +19,17 @@ class BeanFactoryTest {
         assertEquals(helloService.sayHello(), "hello");
     }
 
+    @Test
+    void testGetBeans() {
+        //测试同一个 bean 多次 getBean 返回同一个对象
+        //BeanFactory 对同一个 beanName，是否始终返回同一个实例
+        SimpleBeanFactory beanFactory = new SimpleBeanFactory();
+        beanFactory.registerBean("helloService", new HelloService());
+        Object bean1 = beanFactory.getBean("helloService");
+        Object bean2 = beanFactory.getBean("helloService");
+        //单例测试,同一个 name，多次 getBean是否是同一个对象
+        assertSame(bean1, bean2);//比较是否是同一个对象
+    }
     static class HelloService {
         public String sayHello() {
             System.out.println("hello");
